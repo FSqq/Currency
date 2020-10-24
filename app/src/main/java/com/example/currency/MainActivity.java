b7f02099da887b19fc75d859468321ad4b251246
qqboii <atyxyta@gmail.com>
2020-10-24 23:52:11 +0200

    TextView id added

--- app/src/main/java/com/example/currency/MainActivity.java
+++ app/src/main/java/com/example/currency/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.currency;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvHello = (TextView) findViewById(R.id.tv_hello);
     }
 }
